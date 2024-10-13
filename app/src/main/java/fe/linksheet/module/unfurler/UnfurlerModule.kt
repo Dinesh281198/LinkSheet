8ba3da1c251594703845965d29d064c8d80e6ffd
1fexd <PGFvy85nC@protonmail.com>
2024-10-13 09:06:14 +0200

    feat/wip: Link engine

--- app/src/main/java/fe/linksheet/module/unfurler/UnfurlerModule.kt
+++ app/src/main/java/fe/linksheet/module/unfurler/UnfurlerModule.kt
@@ -3,6 +3,6 @@ package fe.linksheet.module.unfurler
 import me.saket.unfurl.Unfurler
 import org.koin.dsl.module
 
-val unfurlerModule = module {
-    single<Unfurler> { Unfurler() }
+val UnfurlerModule = module {
+    single<Unfurler> { Unfurler(httpClient = get()) }
 }
